dc9f0b10013e32bbf4cec30bd090b803d479431e
Calebe Elias Ribeiro Brim <calebebrim@gmail.com>
2020-08-03 23:16:50 -0300

    .

--- Assets/Scripts/LandGeneration/MeshGenerator.cs
+++ Assets/Scripts/LandGeneration/MeshGenerator.cs
@@ -3,21 +3,21 @@ using UnityEngine;
 public static class MeshGenerator {
   public static MeshData GenerateTerrainMesh (float[, ] heightMap, float heightMultiplier, AnimationCurve heightCurve, int levelOfDetail) {
     AnimationCurve _heightCurve = new AnimationCurve (heightCurve.keys);
-    var width = heightMap.GetLength (0);
-    var height = heightMap.GetLength (1);
-    float topLeftX = (width - 1) / -2f;
-    float topLeftZ = (height - 1) / 2f;
+    var borderedSize = heightMap.GetLength (0);
+    var meshSize = borderedSize - 2;
+    float topLeftX = (borderedSize - 1) / -2f;
+    float topLeftZ = (borderedSize - 1) / 2f;
 
     int meshSimplificationIncrement = (levelOfDetail == 0) ? 1 : levelOfDetail * 2;
-    int verticesPerLine = (width - 1) / meshSimplificationIncrement + 1;
+    int verticesPerLine = (borderedSize - 1) / meshSimplificationIncrement + 1;
     MeshData meshData = new MeshData (verticesPerLine, verticesPerLine);
     int vertexIndex = 0;
 
-    for (int y = 0; y < height; y += meshSimplificationIncrement) {
-      for (int x = 0; x < width; x += meshSimplificationIncrement) {
+    for (int y = 0; y < borderedSize; y += meshSimplificationIncrement) {
+      for (int x = 0; x < borderedSize; x += meshSimplificationIncrement) {
         meshData.vertices[vertexIndex] = new Vector3 (topLeftX + x, _heightCurve.Evaluate (heightMap[x, y]) * heightMultiplier, topLeftZ - y);
-        meshData.uvs[vertexIndex] = new Vector2 (x / (float) width, y / (float) height);
-        if (x < width - 1 && y < height - 1) {
+        meshData.uvs[vertexIndex] = new Vector2 (x / (float) borderedSize, y / (float) borderedSize);
+        if (x < borderedSize - 1 && y < borderedSize - 1) {
           meshData.addTriangle (vertexIndex, vertexIndex + verticesPerLine + 1, vertexIndex + verticesPerLine);
           meshData.addTriangle (vertexIndex + verticesPerLine + 1, vertexIndex, vertexIndex + 1);
         }
